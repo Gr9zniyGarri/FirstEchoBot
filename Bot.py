3e15833628808401ad4ddcabf223965bb5e1d98b
Ivan <curl@gmail.ru>
2019-09-27 12:37:59 +0300

    add hasattr check

--- Bot.py
+++ Bot.py
@@ -8,21 +8,22 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
-list_of_planets = [name for _0, _1, name in ephem._libastro.builtin_planets()]
-
 
 def planet_place(bot, update):
-    user_text = update.message.text.split(" ")
-    user_planet = user_text[1]
-    d = datetime.datetime.now()
-    if user_planet in list_of_planets:
-        const = getattr(ephem, user_planet)(d)
-        result = ephem.constellation(const)
-        bot.sendMessage(update.message.chat_id, result)
-        logging.info(result)
-    else:
-        reply_to_user = "Эта планета или спутник находится вне солнечной системы"
-        bot.sendMessage(update.message.chat_id, reply_to_user)
+    user_text = update.message.text.split()
+    try:
+        user_planet = user_text[1]
+        d = datetime.datetime.now()
+        if hasattr(ephem, user_planet):
+            const = getattr(ephem, user_planet)(d)
+            result = ephem.constellation(const)
+            update.message.reply_text(result)
+            logging.info(result)
+        else:
+            reply_to_user = "Эта планета или спутник находится вне солнечной системы"
+            update.message.reply_text(reply_to_user)
+    except IndexError:
+        update.message.reply_text('Вы не ввели название планеты')
 
 
 def greet_user(bot, update):
